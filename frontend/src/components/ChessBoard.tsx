92969886a9ec78152a94aa114eab150a67e0bb65
Somnath Chataraj <somnathchattaraj5@gmail.com>
2024-05-07 18:08:43 +0530

    Update frontend(Add socket)

---
+++ frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import chessboard from '../assets/ChessBoard.jpeg'
+import { Color, PieceSymbol, Square } from 'chess.js'
+
+const ChessBoard = ({board, socket} : {
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color
+    } | null)[][];
+    socket: WebSocket;
+}) => {
+    const [from, setFrom] = useState(null)
+    const [to, setTo] = useState(null)
+  return (
+    <div className='text-white-200'>
+        {board.map((row, i) => {
+            return (
+                <div key={i} className='flex'>
+                
+                    {row.map((square, j) => {
+                        return (
+                            <div key={j} onClick={()=> {
+                                if (from && to) {
+                                    socket.send(JSON.stringify({type: 'move', payload: {from, to}}))
+                                    setFrom(null)
+                                    setTo(null)
+                                } else if (square) {
+                                    if (!from) {
+                                        // setFrom({i,j})
+                                    } else {
+                                        // setTo(square ? {i,j} : null)
+                                    }
+                                }
+                                
+                            }} className={`w-16 h-16 ${(i+j)%2 == 0 ? 'bg-green-700' : 'bg-green-300'}`}>
+                                <div className='w-full h-full flex justify-center'>
+                                    <div className='h-full flex justify-center flex-col'>
+                                        {square ? square.type : ""}
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        })}
+        {/* <img src={chessboard} alt="Chessboard" className="pl-10 "/> */}
+    </div>
+  )
+}
+
+export default ChessBoard
